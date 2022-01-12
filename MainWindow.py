575b23132097e87e1f070488f32188abdb9b7f92
zhenlee567 <707318815@qq.com>
2022-01-12 17:09:44 +0800

    大修改，将服务端换到识别端

--- MainWindow.py
+++ MainWindow.py
@@ -8,16 +8,15 @@ import screen_capture
 import numpy
 import cv2
 import numpy as np
-import socket
 from MyLogger import MyLogger
-from PyQt5.QtNetwork import QTcpSocket, QHostAddress
+from PyQt5.QtNetwork import QTcpServer, QHostAddress
 from LoginWindow import LoginWindow
 
 class MainWindow(QMainWindow):
     def __init__(self):
         QWidget.__init__(self)
-        self.IP = '127.0.0.1'
-        self.PORT = 8080
+        self.IP = '0.0.0.0'     #监听所有ip
+        self.PORT = 60000       #监听来自60000端口的请求
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.bbox = None
@@ -30,26 +29,23 @@ class MainWindow(QMainWindow):
         self.item = None
         self.scene = QGraphicsScene(self)
         self.log = MyLogger('MainWindowLog.txt')
-        self.sock = QTcpSocket(self)
-        # self.sock.connectToHost(QHostAddress.LocalHost, self.PORT)
-        self.Login_window = LoginWindow()
-        self.Login_window.IP_signal.connect(self.connect_to_host)
-        self.Login_window.show()
-
-    def connect_to_host(self, ip, port):
-        print('connect to host')
-        self.IP = ip
-        self.PORT = port
         self.show()
-        try:
-            self.sock.connectToHost(QHostAddress.LocalHost, self.PORT)
-        except Exception:
-            self.log.logger.error("连接失败")
-            print('xiao ji')
+        # self.sock.connectToHost(QHostAddress.LocalHost, self.PORT)
+        # 建立服务器
+        self.server = QTcpServer(self)
+        # 建立连接
+        if not self.server.listen(QHostAddress(self.IP), self.PORT):
+            self.ui.textEdit.append(self.server.errorString())
+        self.server.newConnection.connect(self.new_socket_slot)
+
+
 
     def write_data_slot(self):
 
         message = self.ui.lineEdit.text()
+        if message == '':
+            message = "识别错误，识别区域中未识别出内容"
+            self.log.logger.error(message)
         print('Client: {}'.format(message))
         datagram = message.encode()
         try:
@@ -160,17 +156,40 @@ class MainWindow(QMainWindow):
         # self.ui.graphicsView.fitInView(QGraphicsPixmapItem(pix))
         # self.ui.graphicsView.show()
 
-    def send_texts(self, text):
-        try:
-            if text is not None:
-                self.client.send(text.encode())
-        except Exception as e:
-            self.log.logger.info('发送失败, 服务器可能未创建或者客户端连接失败')
+    def new_socket_slot(self):
+        self.sock = self.server.nextPendingConnection()
 
-    def create_client(self):
-        try:
-            self.client = socket.socket()
-            self.client.connect((self.IP, self.PORT))
-            self.log
-        except Exception:
-            self.log.logger.info('服务器Ip: ' + str(self.IP) + ' 服务器Port: ' + str(self.PORT) + ' 连接超时!')
+        peer_address = self.sock.peerAddress().toString()
+        peer_port = self.sock.peerPort()
+        news = 'Connected with address {}, port {}'.format(peer_address, str(peer_port))
+        self.log.logger.info(news)
+
+        self.sock.readyRead.connect(lambda: self.read_data_slot(self.sock))
+        # sock.disconnected.connect(lambda: self.disconnected_slot(sock))
+
+        # 3
+    def read_data_slot(self, sock):
+        while sock.bytesAvailable():
+            try:
+                datagram = sock.read(sock.bytesAvailable())
+                message = datagram.decode()
+                #answer = self.get_answer(message).replace('{br}', '\n')
+            except Exception:
+                self.log.logger.error("read_data_slot1: error")
+            try:
+                # new_datagram = answer.encode()
+                #sock.write(new_datagram)
+                self.log.logger.info("[receive message]:"+message)
+                if message == "recognition":
+                    result, img = self.recognition()
+                    if result is not None and img is not None:
+                        str = ''
+                        for i in result:
+                            str += i
+                        self.ui.lineEdit.setText(str)
+                        self.show_image(img)
+                    else:
+                        self.log.logger.info('识别错误，用户未框选')
+                    self.write_data_slot()
+            except Exception:
+                self.log.logger.error("receive message error")
